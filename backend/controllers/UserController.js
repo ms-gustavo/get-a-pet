6c0ae2bf4514430dc5053cc3236c0e79b122ef9c
Gustavo Maia <ms.gustavo@outlook.com>
2023-04-27 14:16:19 -0300

    criacao de usuario

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 module.exports = class UserController {
   static async register(req, res) {
@@ -39,6 +40,25 @@ module.exports = class UserController {
         message: "Por favor, utilize outro e-mail!",
       });
       return;
+    }
+
+    // Create a password
+    const salt = await bcrypt.genSalt(12);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    //Create a user
+    const user = new User({
+      name,
+      email,
+      phone,
+      password: passwordHash,
+    });
+
+    try {
+      const newUser = await user.save();
+      res.status(201).json({ message: "Usuário criado.", newUser });
+    } catch (error) {
+      res.status(500).json({ message: error });
     }
   }
 };

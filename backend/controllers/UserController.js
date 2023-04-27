3107048ec90f5a061ded41893a5731391c473751
Gustavo Maia <ms.gustavo@outlook.com>
2023-04-27 14:32:05 -0300

    criacao de token + funcao logar ao criar usuario

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,3 +1,4 @@
+const createUserToken = require("../helpers/create-user-token");
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
@@ -56,7 +57,8 @@ module.exports = class UserController {
 
     try {
       const newUser = await user.save();
-      res.status(201).json({ message: "Usuário criado.", newUser });
+
+      await createUserToken(newUser, req, res);
     } catch (error) {
       res.status(500).json({ message: error });
     }

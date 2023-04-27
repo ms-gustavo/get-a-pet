57c879b380c606ae9ce815ea54b5ddb8002f4c0b
Gustavo Maia <ms.gustavo@outlook.com>
2023-04-27 14:10:23 -0300

    validacao de usuario

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,6 +2,43 @@ const User = require("../models/User");
 
 module.exports = class UserController {
   static async register(req, res) {
-    res.json("Olá Get A Pet");
+    const { name, email, phone, password, confirmpassword } = req.body;
+
+    // Validations
+    if (!name) {
+      res.status(422).json({ message: "Por favor, insira um nome." });
+      return;
+    }
+    if (!email) {
+      res.status(422).json({ message: "Por favor, insira um email." });
+      return;
+    }
+    if (!phone) {
+      res.status(422).json({ message: "Por favor, insira um telefone." });
+      return;
+    }
+    if (!password) {
+      res.status(422).json({ message: "Por favor, insira uma senha." });
+      return;
+    }
+    if (!confirmpassword) {
+      res.status(422).json({ message: "A confirmação de senha é obrigatória" });
+      return;
+    }
+
+    if (password !== confirmpassword) {
+      res.status(422).json({
+        message: "A senha e a confirmação de senha precisam ser iguais!",
+      });
+    }
+
+    // Check if User Exists
+    const userExists = await User.findOne({ email: email });
+    if (userExists) {
+      res.status(422).json({
+        message: "Por favor, utilize outro e-mail!",
+      });
+      return;
+    }
   }
 };

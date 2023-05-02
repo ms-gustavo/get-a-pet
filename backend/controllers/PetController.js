38c69809371225bb9d5dce0d3b31cba628dcf13d
Gustavo Maia <ms.gustavo@outlook.com>
2023-05-02 15:42:29 -0300

    Funcionalidade de resgatar pet pelo id

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -3,6 +3,7 @@ const Pet = require("../models/Pet");
 //helpers
 const getToken = require("../helpers/get-token");
 const getUserByToken = require("../helpers/get-user-by-token");
+const ObjectId = require("mongoose").Types.ObjectId;
 
 module.exports = class PetController {
   //Errors Message
@@ -97,6 +98,30 @@ module.exports = class PetController {
 
     res.status(200).json({
       pets,
+    });
+  }
+
+  static async getPetById(req, res) {
+    const id = req.params.id;
+
+    if (!ObjectId.isValid(id)) {
+      res.status(422).json({
+        message: `ID inválido!`,
+      });
+      return;
+    }
+
+    // check if pet exists
+    const pet = await Pet.findOne({ _id: id });
+
+    if (!pet) {
+      res.status(404).json({
+        message: `O pet não existe!`,
+      });
+    }
+
+    res.status(200).json({
+      pet,
     });
   }
 };

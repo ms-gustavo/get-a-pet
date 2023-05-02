1e49adb05b5023b11ad592e05be2c8557b6ff5e1
Gustavo Maia <ms.gustavo@outlook.com>
2023-05-02 15:35:29 -0300

    inicio funcionalidade de adocao

--- backend/routes/PetRoutes.js
+++ backend/routes/PetRoutes.js
@@ -12,6 +12,7 @@ router.post(
   PetController.create
 );
 router.get("/", PetController.getAll);
-router.get('/mypets', verifyToken, PetController.getAllUserPets);
+router.get("/mypets", verifyToken, PetController.getAllUserPets);
+router.get("/myadoptions", verifyToken, PetController.getAllUserAdoptions);
 
 module.exports = router;

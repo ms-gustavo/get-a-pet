ac85b58faf776a3297d3984bd8177b4793c5aa89
Gustavo Maia <ms.gustavo@outlook.com>
2023-05-03 12:09:18 -0300

    estilziacao navbar e footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,10 @@ import {
   Navigate,
 } from "react-router-dom";
 
+// Components
+import Navbar from "./components/layouts/Navbar";
+import Footer from "./components/layouts/Footer";
+
 // Pages
 import Home from "./components/pages/Auth/Home";
 import Register from "./components/pages/Auth/Register";
@@ -13,11 +17,13 @@ import Login from "./components/pages/Auth/Login";
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
---
+++ frontend/src/components/layouts/Navbar.js
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import Logo from "../../assets/img/logo.png";
+//css
+import styles from "./Navbar.module.css";
+
+function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.navbar_logo}>
+        <img src={Logo} alt="Get A Pet" />
+        <h2>Get a Pet</h2>
+      </div>
+      <ul>
+        <li>
+          <Link to="/">Adotar</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/register">Cadastrar</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;

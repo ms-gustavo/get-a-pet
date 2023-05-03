018bb88735569ffec858f39c3ccca96386897ab7
Gustavo Maia <ms.gustavo@outlook.com>
2023-05-03 13:49:03 -0300

    criacao formulario componentizado

---
+++ frontend/src/components/forms/Input.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "./Input.module.css";
+
+const Input = ({
+  type,
+  text,
+  name,
+  placeholder,
+  handleOnChange,
+  value,
+  multiple,
+}) => {
+  return (
+    <div className={styles.form}>
+      <label htmlFor={name}>{text}:</label>
+      <input
+        type={type}
+        name={name}
+        id={name}
+        placeholder={placeholder}
+        onChange={handleOnChange}
+        value={value}
+        {...(multiple ? { multiple } : "")}
+      />
+    </div>
+  );
+};
+
+export default Input;
--- frontend/src/components/pages/Auth/Register.js
+++ frontend/src/components/pages/Auth/Register.js
@@ -1,7 +1,49 @@
+import Input from "../../forms/Input";
+
+function handleChange(e) {}
+
 function Register() {
   return (
     <section>
-      <h1>Register</h1>
+      <h1>Cadastrar</h1>
+      <form>
+        <Input
+          text="Nome"
+          type="text"
+          name="name"
+          placeholder="Digite o seu nome"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Telefone"
+          type="text"
+          name="phone"
+          placeholder="Digite o seu telefone"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Email"
+          type="text"
+          name="email"
+          placeholder="Digite o seu e-mail"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Senha"
+          type="password"
+          name="password"
+          placeholder="Digite a sua senha"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Confirmação de senha"
+          type="password"
+          name="confirmpassword"
+          placeholder="Confirme a sua senha"
+          handleOnChange={handleChange}
+        />
+        <input type="submit" value="Cadastrar" />
+      </form>
     </section>
   );
 }

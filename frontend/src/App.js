7d588739611ea000c2d2260c38be5df92b37cbcb
Gustavo Maia <ms.gustavo@outlook.com>
2023-05-03 11:41:05 -0300

    inicializacao components home, login, register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,24 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+
+// Pages
+import Home from "./components/pages/Auth/Home";
+import Register from "./components/pages/Auth/Register";
+import Login from "./components/pages/Auth/Login";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Get a Pet</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 

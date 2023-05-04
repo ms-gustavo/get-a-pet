67e94b3520e4d55c623a137b82e8dea12246c458
Gustavo Maia <ms.gustavo@outlook.com>
2023-05-04 16:22:24 -0300

    condicao de renderizacao na tela se usuario estiver logado

--- frontend/src/components/layouts/Navbar.js
+++ frontend/src/components/layouts/Navbar.js
@@ -1,9 +1,15 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+
 import Logo from "../../assets/img/logo.png";
 //css
 import styles from "./Navbar.module.css";
+// Context
+import { Context } from "../../context/UserContext";
 
 function Navbar() {
+  const { authenticated } = useContext(Context);
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.navbar_logo}>
@@ -14,12 +20,20 @@ function Navbar() {
         <li>
           <Link to="/">Adotar</Link>
         </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/register">Cadastrar</Link>
-        </li>
+        {authenticated ? (
+          <>
+            <p>Logado</p>
+          </>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Cadastrar</Link>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );

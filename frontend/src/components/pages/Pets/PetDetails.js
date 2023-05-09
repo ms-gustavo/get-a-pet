4a5c54fad79c9764d611b165894d59928948aa1f
Gustavo Maia <ms.gustavo@outlook.com>
2023-05-09 16:07:41 -0300

    resgate dos dados do pet no bd

--- frontend/src/components/pages/Pets/PetDetails.js
+++ frontend/src/components/pages/Pets/PetDetails.js
@@ -1,11 +1,23 @@
 import api from "../../../utils/api";
 import { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
-
 import styles from "./PetDetails.module.css";
+// hooks
+import useFlashMessage from "../../../hooks/useFlashMessage";
 
 function PetDetails() {
-  return <h1>Página de pet</h1>;
+  const [pet, setPet] = useState({});
+  const { setFlashMessage } = useFlashMessage();
+  const { id } = useParams();
+  const [token] = useState(localStorage.getItem("token") || "");
+
+  useEffect(() => {
+    api.get(`/pets/${id}`).then((response) => {
+      setPet(response.data.pet);
+    });
+  }, [id]);
+
+  return <h1>{pet.name}</h1>;
 }
 
 export default PetDetails;

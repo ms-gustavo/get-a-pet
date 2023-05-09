e1f40a329dbfa67cba5584c49f45276cfac78306
Gustavo Maia <ms.gustavo@outlook.com>
2023-05-08 23:08:50 -0300

    pegando dados dos pets do usuario no banco de dados

--- frontend/src/components/pages/Pets/Mypets.js
+++ frontend/src/components/pages/Pets/Mypets.js
@@ -1,7 +1,28 @@
+import api from "../../../utils/api";
+
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import RoundedImage from "../../layouts/RoundedImage";
+// hooks
+import useFlashMessage from "../../../hooks/useFlashMessage";
+
 function MyPets() {
   const [pets, setPets] = useState([]);
+  const [token] = useState(localStorage.getItem("token") || "");
+  const { setFlashMessage } = useFlashMessage();
+
+  useEffect(() => {
+    api
+      .get("/pets/mypets", {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(token)}`,
+        },
+      })
+      .then((response) => {
+        setPets(response.data.pets);
+      });
+  }, [token]);
+
   return (
     <section>
       <div>
